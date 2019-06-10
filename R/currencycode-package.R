6909193d57bb7bb1cf7d379ac0399b3580d5c62b
Kasia Kulma <kkulma@mango-solutions.com>
2019-06-10 22:40:53 +0100

    drafted R/currencycode-package.R

---
+++ R/currencycode-package.R
@@ -0,0 +1,22 @@
+#' Convert country codes or country names
+#'
+#' The \code{countrycode} function can convert to and from several different
+#' country coding schemes.  It uses regular expressions to convert country
+#' names (e.g. Sri Lanka) into any of those coding schemes, or into
+#' standardized country names in several languages.  It can create variables
+#' with the name of the continent and/or several regional groupings to which
+#' each country belongs.
+#'
+#' Type ?codelist to get a list of available origin and destination
+#' codes.
+#'
+#' @name currencycode-package
+#' @aliases currencycode-package
+#' @title Convert Country Codes or Country Names
+#' @author Kasia Kulma \email{katarzyna.kulma@gmail.com}
+#' @references
+#'     \url{https://r-tastic.co.uk/}
+#'     \url{https://github.com/KKulma/currencycode}
+#' @docType package
+#' @keywords package
+NULL
