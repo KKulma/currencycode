20e9094b9f80562d63deb7f62969151d43586032
Kasia Kulma <kkulma@mango-solutions.com>
2019-06-10 22:31:59 +0100

    added currency_list description

---
+++ R/currency_list.R
@@ -0,0 +1,18 @@
+#' Currency names and codes and names of 200+ countries
+#'
+#' A dataset containing the currency name, codes and other country attributes of almost 250 coutries.
+#'
+#' @format A data frame with 249 rows and 7 variables:
+#' \describe{
+#'   \item{countrycode_2}{2-letter country code}
+#'   \item{countrycode_3}{3-letter country code}
+#'   \item{countrycode_num}{country numeric code}
+#'   \item{country_name}{common counry name}
+#'   \item{country_fullname}{full country name}
+#'   \item{currency_code}{currency ISO code}
+#'   \item{currency_name}{full currency name}
+#'
+#'
+#' }
+#' @source \code{countrycode} package \url{https://github.com/vincentarelbundock/countrycode/}
+"currency_list"
